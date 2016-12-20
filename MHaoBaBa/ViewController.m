52eb4673d8468a4db0de5843f166b853c4ab4d8c
limingming <liyuemin.1986@gmail.com>
2016-12-20 10:13:29 +0800

    for all

--- MHaoBaBa/ViewController.m
+++ MHaoBaBa/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-#import "OCGumbo+Query.h"
-#import "OCGumbo.h"
+#import "TFHpple.h"
+#import "TFHppleElement.h"
 
 @interface ViewController ()
 
@@ -18,27 +18,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSError *error = nil;
-    NSURL *xcfURL = [NSURL URLWithString:@"http://www.guiderank.org/ranking/14606859879904513908"];
-    NSString *htmlString = [NSString stringWithContentsOfURL:xcfURL encoding:NSUTF8StringEncoding error:&error];
-    OCGumboDocument *document = [[OCGumboDocument alloc] initWithHTMLString:htmlString];
-    NSLog(@"%@",document.Query(@"#v-ranking"));
-    NSLog(@"%@",document.title);
-    NSLog(@"%@",document.publicID);
-    NSLog(@"%@",document.systemID);
-    NSLog(@"%@",document.rootElement);
-    NSLog(@"%@",document.head);
-    NSLog(@"%@",document.body);
-    NSLog(@"%@",document.nodeName);
-    NSLog(@"%@",document.nodeValue);
-    NSLog(@"%@",document.childNodes);
-    NSLog(@"%@",document.parentNode);
-          
-    NSLog(@"%@",document.childNodes);
-    OCQueryObject *element = document.Query(@"body").find(@".ranking-banner").find(@".container");
-    NSLog(@"%@",element);
     
-    // Do any additional setup after loading the view, typically from a nib.
+//    NSError *error = nil;
+//    NSURL *xcfURL = [NSURL URLWithString:@"http://www.guiderank.org/ranking/14606859879904513908"];
+//    NSString *htmlString = [NSString stringWithContentsOfURL:xcfURL encoding:NSUTF8StringEncoding error:&error];
+//    
+//    //NSData  * data = [NSData dataWithContentsOfFile:htmlString];
+//    NSData *htmlData = [htmlString dataUsingEncoding:NSUTF8StringEncoding];
+//    
+//    TFHpple * doc= [[TFHpple alloc] initWithHTMLData:htmlData];
+//    NSArray * elements  = [doc searchWithXPathQuery:@"//div[@id='v-ranking-content']//div[1]//ul"];
+//    
+//    //NSArray *elements = [xpathParser searchWithXPathQuery:@"//div[@id='news']//div//div[2]//h3//a[1]"];
+//    
+//    for(TFHppleElement * element in elements){
+//        NSLog(@"%@",[element text]);
+//        NSLog(@"%@",[element tagName]);
+//        NSLog(@"%@",[element attributes]);
+//        NSLog(@"%@",[element children]);
+//        NSLog(@"%@",[element content]);
+//        for (TFHppleElement *childrenElement in element.children){
+//            NSLog(@"%@",[childrenElement text]);
+//            NSLog(@"%@",[childrenElement tagName]);
+//            NSLog(@"%@",[childrenElement attributes]);
+//            NSLog(@"%@",[element content]);
+//            
+//        }
+//        //        [element text];                       // The text inside the HTML element (the content of the first text node)
+//        //        [element tagName];                    // "a"
+//        //        [element attributes];                 // NSDictionary of href, class, id, etc.
+//        //        [element objectForKey:@"href"];       // Easy access to single attribute
+//        //        [element firstChildWithTagName:@"b"];
+//    } // The first "b" child node
+
 }
 
 
